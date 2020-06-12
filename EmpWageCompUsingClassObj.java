ad14c6f38006521d104b491832517a74b4ba38c6
nelson26-shruti-mom <nelsonwasekar555@gmail.com>
2020-06-12 19:28:02 +0530

    Added : Employee Part Time Wage Functionality

--- EmpWageCompUsingClassObj.java
+++ EmpWageCompUsingClassObj.java
@@ -1,37 +1,35 @@
 public class EmpWageCompUsingClassObj
 {
 
+           int Salery = 0;
+           int empHrs = 0;
 
-           int Salery=0;
-
-           private int WAGE_PER_HR=20;
-           private int FULL_DAY_PER_HR=8;
-           private int IS_PRESENT=1;
-
-           // private final int WAGE_PER_HR=20;
-           // private final int FULL_DAY_PER_HR=8;
-           // private final int IS_PRESENT=1;
-
-           // private static final int WAGE_PER_HR=20;
-           // private static final int FULL_DAY_PER_HR=8;
-           // private static final int IS_PRESENT=1;
-
+           private int IS_PART_TIME = 1 ;
+           private int IS_FULL_TIME = 2 ;
+           private int WAGE_PER_HR = 20;
 
 
       public void getRandom()
          {
-           double empCheck = Math.floor(Math.random() * 10) % 2;
-           if  (IS_PRESENT == empCheck )
+           double empCheck = Math.floor(Math.random() * 10) % 3;
+           if  (IS_FULL_TIME == empCheck )
              {
                 System.out.println("Employee is Present");
-                Salery = FULL_DAY_PER_HR*WAGE_PER_HR;
-                System.out.println("Salery : "+Salery);
+                   empHrs = 4;
+             }
+           else if (IS_PART_TIME == empCheck )
+             {
+              System.out.println("Employee is Present");
+                  empHrs = 8;
              }
            else
              {
               System.out.println("Employee is Abscent");
              }
+          Salery = empHrs * WAGE_PER_HR;
+          System.out.println("Salery : "+Salery);
          }
+
 
       public static void main(String[ ] args)
          {
